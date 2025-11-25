7c5737a9e608d319136b2c85845e268fdb15c8b2
heardchef33 <prachathornth@gmail.com>
2025-11-25 14:45:34 +0700

    addedspark session function here so it doesnt have to be repeated in every file

--- config/spark_config.py
+++ config/spark_config.py
@@ -1,3 +1,6 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
 def get_spark_config(): 
     """
     returns spark configuration for development based on my local machine 
@@ -12,3 +15,19 @@ def get_spark_config():
         "spark.driver.maxResultSize": "2g"
     }
 
+def create_spark_session(): # repeated for simplicity during development; will be removed later 
+    """
+    create spark session for development 
+    """
+    config = get_spark_config()
+
+    conf = SparkConf()
+
+    for con, settings in config.items(): 
+        conf.set(con, settings)
+
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    return spark
