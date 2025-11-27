e2d5b6014f87fa2431f9756464df19cb562bcf43
heardchef33 <prachathornth@gmail.com>
2025-11-27 11:07:19 +0700

    using pandas udf causes spark to crash; manually convert to vectors instead

--- src/transformation/embeddings.py
+++ src/transformation/embeddings.py
@@ -1,11 +1,6 @@
-import pandas as pd 
-from pyspark.sql import functions as F, DataFrame
-from pyspark.sql.functions import pandas_udf
 
-from sentence_transformers import SentenceTransformer, util
-
-from clean import miscalleneous_cleaning
 from config.spark_config import create_spark_session
+from sentence_transformers import SentenceTransformer
 
 
 ## apply the sentence transformer 
@@ -16,27 +11,30 @@ from config.spark_config import create_spark_session
 ## convert the abstract column in series (not tokenised) each row has a abstract string 
 ## each string is a given a embedding which is return as a array of floats 
 
-def to_series(df: DataFrame) -> pd.Series: 
-    return df.select(
-        F.col("abstract")
-    ).toPandas()['abstract']
-
-@pandas_udf("array<float>")
-def embedder(abstract: pd.Series) -> pd.Series:
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embeddings = model.encode(abstract.to_list(), show_progress_bar=True)
-    return embeddings
-
+# def to_series(df: DataFrame) -> pd.Series: 
+#     return df.select(
 
 if __name__ == "__main__":
 
-    PARQUET_FOLDER = '/Users/thananpornsethjinda/Desktop/rkg/data/staging'
-
     spark = create_spark_session()
 
-    df = miscalleneous_cleaning(spark, PARQUET_FOLDER)
+    model = SentenceTransformer('all-MiniLM-L6-v2') 
 
-    to_series(df)
+    for year in range(1991, 2026):
+        year_df = spark.read.parquet(f"data/staging/pub_year={year}")
+        year_pd = year_df.select("id", "abstract").toPandas()
+        
+        # generate embeddings
+        embeddings = model.encode(year_pd['abstract'].tolist(), batch_size=32)
+        year_pd['embedding'] = embeddings.tolist()
+        
+        # convert back to parquet to reduce memory contraint 
+        year_pd[['id', 'embedding']].to_parquet(f"data/embeddings/{year}_embeddings.parquet")
+        
+        print(f"Year {year} complete")
+    
+
+
 
 
     
